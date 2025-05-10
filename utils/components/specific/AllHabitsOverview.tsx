a4ea53239732c9bd62bbc5a75dd2258b5882e99c
Pratham S <snehi.pratham@gmail.com>
2025-05-09 20:42:02 -0500

    before adding gluestack ui

--- utils/components/specific/AllHabitsOverview.tsx
+++ utils/components/specific/AllHabitsOverview.tsx
@@ -1,6 +1,22 @@
 import { useTheme } from "@/utils/theme/ThemeContext";
 import { Ionicons } from "@expo/vector-icons";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SheetManager } from "react-native-actions-sheet";
+
+placement
+
+bottom end
+
+bottom end
+import { Button, ButtonText } from "@/components/ui/button"
+import { Menu, MenuItem, MenuItemLabel } from "@/components/ui/menu"
+import {
+  Icon,
+  AddIcon,
+  GlobeIcon,
+  PlayIcon,
+  SettingsIcon,
+} from "@/components/ui/icon"
 
 export default function AllHabitsOverview({
   allHabitsArray,
@@ -14,7 +30,7 @@ export default function AllHabitsOverview({
     <View style={styles.allHabitsContainer}>
       {allHabitsArray.map((habitItem, index) => {
         return (
-          <View style={styles.habitCard} key={`${habitItem.habitName}-card`}>
+          <View style={styles.habitCard}>
             <Ionicons name="today" style={styles.habitIcon} size={24} />
             <View style={styles.habitCardText}>
               <Text style={styles.habitName}>{habitItem.habitName}</Text>
@@ -32,9 +48,59 @@ export default function AllHabitsOverview({
                 );
               })}
             </View>
+            <TouchableOpacity
+              style={styles.habitOptions}
+              onPress={() => {
+                console.log("things i should of said", habitItem);
+                SheetManager.show("example-sheet", {
+                  payload: {
+                    sheetType: "habitItem",
+                    habitItem: {
+                      habit: habitItem,
+                      habitIndex: index,
+                    },
+                  },
+                });
+              }}
+            >
+              <Ionicons
+                name="ellipsis-vertical-outline"
+                size={20}
+                color={theme.colors.textSecondary}
+              />
+            </TouchableOpacity>
           </View>
         );
       })}
+      <Menu
+        placement="bottom end"
+        offset={5}
+        disabledKeys={["Settings"]}
+        trigger={({ ...triggerProps }) => {
+          return (
+            <Button {...triggerProps}>
+              <ButtonText>Menu</ButtonText>
+            </Button>
+          );
+        }}
+      >
+        <MenuItem key="Add account" textValue="Add account">
+          <Icon as={AddIcon} size="sm" className="mr-2" />
+          <MenuItemLabel size="sm">Add account</MenuItemLabel>
+        </MenuItem>
+        <MenuItem key="Community" textValue="Community">
+          <Icon as={GlobeIcon} size="sm" className="mr-2" />
+          <MenuItemLabel size="sm">Community</MenuItemLabel>
+        </MenuItem>
+        <MenuItem key="Plugins" textValue="Plugins">
+          <Icon as={PlayIcon} size="sm" className="mr-2" />
+          <MenuItemLabel size="sm">Plugins</MenuItemLabel>
+        </MenuItem>
+        <MenuItem key="Settings" textValue="Settings">
+          <Icon as={SettingsIcon} size="sm" className="mr-2" />
+          <MenuItemLabel size="sm">Settings</MenuItemLabel>
+        </MenuItem>
+      </Menu>
     </View>
   );
 }
@@ -79,6 +145,9 @@ function createStyles(theme: any) {
       borderRadius: 4,
       backgroundColor: theme.colors.primary,
       marginHorizontal: 2,
+    },
+    habitOptions: {
+      alignSelf: "center",
     },
   });
 }
